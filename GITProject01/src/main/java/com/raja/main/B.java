1d75caadef93f3fcbe875bf01c5c4ca40dec9a2c
rajur <rajur@MSI>
2025-11-12 16:46:11 +0530

    TL added sub method to b.java

--- GITProject01/src/main/java/com/raja/main/B.java
+++ GITProject01/src/main/java/com/raja/main/B.java
@@ -5,6 +5,10 @@ public class B {
 	public int sum(int a, int b) {
 		return a+b;
 	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
