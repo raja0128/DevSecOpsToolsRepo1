48c0c8aaa94b67bd8575e148046bd3431df5c3a0
raja <gurupunya.1@gmail.com>
2025-11-13 15:51:11 +0530

    Dev added debitAmount method

--- GITProject08/src/main/java/com/raja/main/BankingService.java
+++ GITProject08/src/main/java/com/raja/main/BankingService.java
@@ -5,4 +5,8 @@ public class BankingService {
 	public String transfer(Long accNo, int pwd) {
 		return "amount transfered from " + accNo + " succesfully";
 	}
+	
+	public String debitAmount(Long accNo, double amt) {
+		return amt + " Amount debited from " + accNo + " successfully";
+	}
 }
