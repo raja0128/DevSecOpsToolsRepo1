534c95c3ec77eeb9ce642742e19f3aa879cb03c6
rajur <rajur@MSI>
2025-11-13 14:14:58 +0530

    Dev changed App.java to create conflict

--- GITProject07/src/main/java/com/raja/service/App.java
+++ GITProject07/src/main/java/com/raja/service/App.java
@@ -3,5 +3,7 @@ package com.raja.service;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hi Hello App");
+        
+        System.out.println("App is App only");
     }
 }
